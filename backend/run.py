8d0c94d931675c2dd6507e632ffcd432ece531bb
Pranshu Namdeo <namdeopranshu8@gmail.com>
2026-05-27 22:22:35 +0530

    fixed the bug in run.py

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,13 @@
 import uvicorn
 
-uvicorn.run(
-    "app.main:app",
-    host="127.0.0.1",
-    port=8000,
-    reload=True
-)
+def main() -> None:
+    uvicorn.run(
+        "app.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
